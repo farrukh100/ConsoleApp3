029110c07885307426aff2b6c6d4a16b046dfdaa
farrukh100 <farrukhcontour@gmail.com>
2019-11-12 19:50:31 +0500

    changes done

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("First case updated");
+            Console.WriteLine("Second case updated");
             Console.ReadLine();
         }
     }
